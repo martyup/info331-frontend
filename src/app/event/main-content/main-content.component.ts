830605e143595d4e42ee9cd6404902bde691cafc
julesVega <trym_helle@hotmail.com>
2017-11-08 13:52:54 +0100

    misc components

--- src/app/event/main-content/main-content.component.ts
+++ src/app/event/main-content/main-content.component.ts
@@ -14,7 +14,7 @@ results:string[];
 
   ngOnInit(): void {
     // Make the HTTP request:
-    this.http.get('localhost:3000/api/events/1000/images').subscribe(data => {
+    this.http.get('/api/events/1000/images').subscribe(data => {
       // Read the result field from the JSON response.
       this.results = data['results'];
       console.log(this.results);
--- src/app/pictures.service.ts
+++ src/app/pictures.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
+
 export class PicturesService {
 
-  constructor() { }
+  serverUrl:string = "http://localhost:3000/api/events/1000/images";
+
+  constructor(private http:Http) { }
 
   //get pictures fra server
-  
+  // getEventPictures(){
+  //   this.http.get(this.serverUrl);
+  // }
 
 }
